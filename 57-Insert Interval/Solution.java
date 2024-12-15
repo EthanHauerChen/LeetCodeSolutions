e581cf00a312efb3f76d48141f5678ecbeb43de1
Ethan <ethanhchen01@gmail.com>
2024-12-15 02:11:14 -0600

    added #57 Insert Interval. not implemented yet

---
+++ 57-Insert Interval/Solution.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/insert-interval/description/
+
+class Solution {
+    /**
+     * initial observations:
+     * probably binary search since it is a sorted list, then check neighboring elements to see if merge is needed
+     */
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+    }
+}
