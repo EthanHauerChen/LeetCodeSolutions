288bb978f98cf077f757f3409a5d441b7fb98918
Ethan <ethanhchen01@gmail.com>
2024-12-20 22:17:51 -0600

    finished tabulation, but every single one of my solutions is very slow, need to look into why

--- 70-Climbing Stairs/Solution.java
+++ 70-Climbing Stairs/Solution.java
@@ -1,66 +1,102 @@
-//https://leetcode.com/problems/climbing-stairs/
-
 class Solution {
-    public int climbStairs(int n) {
-        /*closed form solution (fibonacci(n+1)) 
-        double gRatio = (1 + Math.pow(5, .5)) / 2;
-        double conjugate = (1 - Math.pow(5, .5)) / 2;
-        return (int) ((Math.pow(gRatio, n + 1) - Math.pow(conjugate, n + 1)) / Math.pow(5, .5));
+    /**
+     * the given examples mislead you into thinking that you can just start with the
+     * highest denomination of coin (that is still less than the sum) with a greedy algorithm 
+     * but that is not the case for coin sets such as [1, 3, 4] where you are trying to get the sum of 6, for example
+     */
+    public int coinChange(int[] coins, int amount) {
+        //my recursive algorithms compute coinChange(amount-denom), for every denom in coins. 
+        //if every time the method is called and coinChange(amount-denom) is computed using the smallest denom, then it would quite inefficient,
+        //so sorting helps me start from the largest denom instead of potentially a small denom
+        Arrays.sort(coins);
+
+        /*normal recursion 
+        return coinChangeRecurse(coins, amount); */
+
+        /*memoization 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        return coinChangeMemo(coins, amount, map);
         */
 
-        /*memoization method: Map<Integer, Integer> waysToClimbStairs = new HashMap<>();
-        return getClimbStairs(n, waysToClimbStairs);
-        */
-
-        /*tabulation method
-        int[] tabulationTable = new int[n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            switch(i) {
-                case 0:
-                case 1: 
-                    tabulationTable[i] = 1;
-                    continue;
-                default:
-                    break;
-            }
-
-            tabulationTable[i] = tabulationTable[i-1] + tabulationTable[i-2];
-        }
-        
-        return tabulationTable[n];
-        */
-
-        /*tabulation with space optimization */
-        int prev = 0;
-        int current = 1;
-        for (int i = 0; i < n + 1; i++) {
-            switch(i) {
-                case 0:
-                case 1: 
-                    prev = current = 1;
-                    continue;
-                default:
-                    break;
-            }
-            
-            int temp = current;
-            current += prev;
-            prev = temp;
-        }
-        
-        return current;
+        /*tabulation */
+        return coinChangeTab(coins, amount);
     }
 
-    /*memoization method 
-    public int getClimbStairs(int n, Map<Integer, Integer> memo) {
-        if (n < 2) return 1;
-        if (n == 2) return 2;
-        Integer result = memo.get(n);
-        if (result != null) return result;
+    /* normal recursion. works for the 3 small test cases, but is seemingly too slow for the other test cases 
+    public int coinChangeRecurse(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
 
-        result = getClimbStairs(n - 1, memo) + getClimbStairs(n - 2, memo);
-        memo.put(n, result);
-        return result;
+        int min = Integer.MAX_VALUE;
+        boolean allNegative = true;
+
+        for (int i = coins.length-1; i > -1; i--) {
+            if (amount == coins[i]) return 1;
+            int nextCoinChangeIter = coinChangeRecurse(coins, amount-coins[i]);
+
+            if (nextCoinChangeIter > -1) allNegative = false;
+            else continue;
+
+            min = Math.min(min, 1+nextCoinChangeIter);
+        }
+
+        if (allNegative) return -1;
+        return min;
     }
     */
+
+    /* memoization 
+    public int coinChangeMemo(int[] coins, int amount, HashMap<Integer, Integer> map) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+
+        Integer value = map.get(amount);
+        if (value != null) return value;
+
+        int min = Integer.MAX_VALUE;
+        boolean allNegative = true;
+
+        for (int i = coins.length-1; i > -1; i--) {
+            if (amount == coins[i]) return 1;
+            int nextCoinChangeIter = coinChangeMemo(coins, amount-coins[i], map);
+
+            if (nextCoinChangeIter > -1) allNegative = false;
+            else continue;
+
+            min = Math.min(min, 1+nextCoinChangeIter);
+        }
+
+        if (allNegative) {
+            map.put(amount, -1);
+            return -1;
+        }
+        map.put(amount, min);
+        return min;
+    } */
+
+    /*tabulation */
+    public int coinChangeTab(int[] coins, int amount) {
+        if (amount == 0) return 0;
+
+        int[] min = new int[amount+1]; 
+        Arrays.fill(min, -1);
+
+        for (int i = 0; i < coins.length; i++) 
+            if (coins[i] <= amount) min[coins[i]] = 1;
+
+        for (int i = 1; i < amount+1; i++) {
+            if (min[i] == -1) continue;
+            for (int j = coins.length-1; j > -1; j--) {
+                if (i+coins[j] > -1 && i+coins[j] <= amount) {
+                    if (min[i+coins[j]] == -1) {
+                        min[i+coins[j]] = min[i] + 1;
+                        System.out.println("min[" + (i+coins[j]) + "] = " + min[i+coins[j]]);
+                    }
+                    else if (min[i+coins[j]] != -1) min[i+coins[j]] = Math.min(min[i+coins[j]], min[i]+1);
+                }
+            }
+        }
+
+        return min[amount];
+    }
 }
