4e37f52fbd35a047feb53185bec41eb8efc8b0eb
Ethan <ethanhchen01@gmail.com>
2025-06-11 21:00:23 -0500

    implemented version without StringBuilder. next implement version using String.split()

--- 151-Reverse Words in a String/Solution.java
+++ 151-Reverse Words in a String/Solution.java
@@ -8,7 +8,7 @@ class Solution {
         using "String result" instead of "StringBuilder result" and using "+=" instead of using "append"
         was extremely slow, and LeetCode's Analyze Complexity functionality (who knows if this is accurate or not)
         showed that it was O(n^2), while the solution using StringBuilder seemed to run much faster (couldn't analyze again since it's only 1 analysis per day)
-         */
+         
         StringBuilder result = new StringBuilder();
         char[] chars = s.toCharArray();
         for (int i = chars.length-1; i > -1; i--) {
@@ -25,9 +25,38 @@ class Solution {
         }
 
         return result.toString().substring(0, result.length()-1);
+        */
 
         /**Same solution as above but not using StringBuilder and maybe using a char array as the result variable instead
         TODO
         */
+        String resultString;
+        char[] result = new char[s.length()+1];
+        char[] chars = s.toCharArray();
+        int resultCounter = 0;
+        for (int i = chars.length-1; i > -1; i--) {
+            if (chars[i] == ' ' || i == 0) {
+                int startIndex = chars[i] == ' ' ? 1 : 0;
+                int counter = i + startIndex;
+                while (counter < chars.length && chars[counter] != ' ') {
+                    result[resultCounter] = chars[counter];
+                    resultCounter++;
+                    counter++;
+                }
+                if (counter > i+startIndex) {
+                    result[resultCounter] = ' ';
+                    resultCounter++;
+                }
+                counter = 0;
+            }
+        }
+
+        //delete trailing spaces and nulls
+        int counter = result.length - 1;
+        while (!Character.isLetterOrDigit(result[counter])) {
+            counter--;
+        }
+        resultString = new String(result);
+        return resultString.substring(0, counter+1);
     }
 }
