486b90a531ca0119a8520fd407f6c0e0963bd3c8
EthanHauerChen <ethanhchen01@gmail.com>
2025-09-12 16:33:18 -0500

    fixed typo

--- 1010-Pairs of Songs/Solution.java
+++ 1010-Pairs of Songs/Solution.java
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+//https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
 import java.util.HashMap;
 
 class Solution {
